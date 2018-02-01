3d99584d26a440e25433cb92e2d8607aa282f063
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-01 15:15:58 +0530

    essential packages, media i/o, video i/o, support packages etc.

--- ubuntu_desktop_setup.sh
+++ ubuntu_desktop_setup.sh
@@ -3,3 +3,18 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
+
+# Essential packages
+sudo apt install -y build-essential g++ cmake git synaptic gdebi alien checkinstall zip unzip
+
+# Media I/O
+sudo apt install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+
+# Video I/O and player
+sudo apt install -y vlc browser-plugin-vlc libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+
+# Other support packages
+sudo apt install -y redshift redshift-gtk unity-tweak-tool gparted
+
+# Python related packages
+sudo apt install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
