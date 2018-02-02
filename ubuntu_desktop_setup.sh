6113680eacb8386935ec91bb4515f59a0abe7f54
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-03 02:37:21 +0530

    need modification

--- ubuntu_desktop_setup.sh
+++ ubuntu_desktop_setup.sh
@@ -7,13 +7,18 @@ sudo apt -y dist-upgrade
 sudo apt -y autoremove
 
 # Essential packages
-sudo apt install -y build-essential g++ cmake git libpq-dev synaptic gdebi alien checkinstall zip unzip
+sudo apt install -y build-essential g++ cmake git libpq-dev synaptic \
+                    gdebi alien checkinstall zip unzip
 
 # Media I/O
-sudo apt install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo apt install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev \
+                    libjasper-dev libopenexr-dev libgdal-dev
 
 # Video I/O and player
-sudo apt install -y vlc browser-plugin-vlc libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
+sudo apt install -y vlc browser-plugin-vlc libdc1394-22-dev libavcodec-dev \
+                    libavformat-dev libswscale-dev libtheora-dev libvorbis-dev \
+                    libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev \
+                    libopencore-amrwb-dev libv4l-dev libxine2-dev
 
 # Other support packages
 sudo apt install -y redshift redshift-gtk unity-tweak-tool gparted sshfs putty filezilla
