512c1e0c4fe25ed98ef8636e709e9405c40dc9c8
Mrinal Sinha <themrinalsinha@gmail.com>
2018-02-02 14:24:35 +0530

    adding sshfs putty and filezilla

--- ubuntu_desktop_setup.sh
+++ ubuntu_desktop_setup.sh
@@ -16,7 +16,7 @@ sudo apt install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev l
 sudo apt install -y vlc browser-plugin-vlc libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
 # Other support packages
-sudo apt install -y redshift redshift-gtk unity-tweak-tool gparted
+sudo apt install -y redshift redshift-gtk unity-tweak-tool gparted sshfs putty filezilla
 
 # Python related packages
 sudo apt install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
